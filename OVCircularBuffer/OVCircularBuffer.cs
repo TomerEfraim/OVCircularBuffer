5a192f0c28c3c60769340db66d04fb76e512bc17
TomerEfraim <thetomer92@gmail.com>
2019-06-30 19:02:48 +0300

    Added visual studio project files

---
+++ OVCircularBuffer/OVCircularBuffer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OVCircularBuffer
+{
+    public class OVCircularBuffer<T>
+    {
+        protected readonly T[] _buffer;
+        protected int _head;
+        protected int _tail;
+        protected readonly object _lock = new object();
+
+        public OVCircularBuffer(int bufferSize)
+        {
+            if (bufferSize < 1)
+            {
+                throw new ArgumentOutOfRangeException("CircularBuffer - constructor exception: buffer size should be > 0");
+            }
+            _buffer = new T[bufferSize];
+            BufferSize = bufferSize;
+            _head = bufferSize - 1;
+            _tail = 0;
+            Count = 0;
+        }
+
+        public int Count { get; protected set; }
+
+        public int BufferSize { get; protected set; }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return Count == 0;
+            }
+        }
+
+        public bool IsFull
+        {
+            get
+            {
+                return Count == BufferSize;
+            }
+        }
+
+        protected virtual int NextPosition(int currPosition)
+        {
+            return (currPosition + 1) % BufferSize;
+        }
+
+        public virtual bool TryDequeue(out T dequeued)
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    if (IsEmpty)
+                    {
+                        dequeued = default;
+                        return false;
+                    }
+                    dequeued = _buffer[_tail];
+                    _tail = NextPosition(_tail);
+                    Count--;
+                    return true;
+                }
+                catch (Exception)
+                {
+                    dequeued = default;
+                    return false;
+                }
+            }
+        }
+
+        public virtual void Enqueue(T toAdd)
+        {
+            lock (_lock)
+            {
+                _head = NextPosition(_head);
+                _buffer[_head] = toAdd;
+                if (IsFull)
+                {
+                    _tail = NextPosition(_tail);
+                }
+                else
+                {
+                    Count++;
+                }
+            }
+        }
+
+        public virtual bool TryPeek(out T peeked)
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    if (IsEmpty)
+                    {
+                        peeked = default;
+                        return false;
+                    }
+                    peeked = _buffer[_tail];
+                    return true;
+                }
+                catch (Exception)
+                {
+                    peeked = default;
+                    return false;
+                }
+            }
+        }
+
+        public virtual T[] ToArray()
+        {
+            lock (_lock)
+            {
+                T[] retArray = new T[Count];
+                int currPos = _tail;
+                for (int i = 0; i < Count; i++)
+                {
+                    retArray[i] = _buffer[currPos];
+                    currPos = NextPosition(currPos);
+                }
+                return retArray;
+            }
+        }
+
+        public virtual void Clear()
+        {
+            lock (_lock)
+            {
+                for (int i = 0; i < BufferSize; i++)
+                {
+                    _buffer[i] = default;
+                }
+                _head = BufferSize - 1;
+                _tail = 0;
+                Count = 0;
+            }
+        }
+    }
+}
---
+++ OVCircularBufferTests/OVCircularBufferTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OVCircularBuffer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OVCircularBuffer.Tests
+{
+    [TestClass()]
+    public class OVCircularBufferTests
+    {
+        [TestMethod()]
+        public void Test1()
+        {
+            OVCircularBuffer<string> instance = new OVCircularBuffer<string>(3);
+
+            string[] data = { "This", "is" };
+
+            instance.Enqueue(data[0]);
+            instance.Enqueue(data[1]);
+
+            bool result = instance.TryDequeue(out string output0);
+            Assert.AreEqual(result, true);
+            Assert.AreEqual(data[0], output0);
+
+            result = instance.TryDequeue(out string output1);
+            Assert.AreEqual(result, true);
+            Assert.AreEqual(data[1], output1);
+
+            Assert.IsTrue(instance.IsEmpty);
+        }
+
+        public static void ProduceLoop(object obj)
+        {
+            if (obj is OVCircularBuffer<int> buffer)
+            {
+                int i = 0;
+                while (true)
+                {
+                    buffer.Enqueue(i);
+                    //Console.WriteLine("enqueued " + i.ToString());
+                    i++;
+                    Task.Delay(1000).Wait(); // simulate fps
+                }
+            }
+        }
+
+        public static void ConsumeLoop(object obj)
+        {
+            if (obj is OVCircularBuffer<int> buffer)
+            {
+                int expected = 0;
+                while (true)
+                {
+                    if (buffer.TryDequeue(out int i))
+                    {
+                        if (expected == i)
+                        {
+                            //Console.WriteLine("dequeued " + i.ToString());
+                            expected++;
+                            Task.Delay(1000).Wait(); // sumulate processing time
+                        }
+                        else
+                        {
+                            // missed frames...
+                        }
+                    }
+                    else
+                    {
+                        // empty buffer....
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void Test2()
+        {
+            OVCircularBuffer<int> buffer = new OVCircularBuffer<int>(10);
+
+            Task produceTask = new Task(ProduceLoop, buffer);
+
+            Task consumeTask = new Task(ConsumeLoop, buffer);
+
+            produceTask.Start();
+
+            Task.Delay(100).Wait();
+
+
+            consumeTask.Start();
+
+
+
+            produceTask.Wait();
+            consumeTask.Wait();
+        }
+    }
+}
